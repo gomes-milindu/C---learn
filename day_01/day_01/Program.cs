4850e85bca5f0926bfb957ed25567cb0de7a18fc
Milindu Gomes <gomesmilindu@gmail.com>
2025-05-27 09:29:38 +0530

    day_01 folder uploaded

---
+++ day_01/day_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            // this is coomment
+
+            // declare variables
+
+            int num1 = 10;
+            double num2 = 5.5;
+            string num3 = "ABC";
+            bool num4 = true;
+            char num5 = 'A';
+
+            int myInt, myInt2;
+
+
+        }
+    }
+}
