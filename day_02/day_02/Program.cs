913894af833f3663b0f06c5d484de45c3478c99a
Milindu Gomes <gomesmilindu@gmail.com>
2025-05-28 22:32:55 +0530

    day2_uploade

---
+++ day_02/day_02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            double num2 = 10.35;
+            string num3 = "ABC";
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+            // Arithmetic Operator
+
+
+            // +  - * / %
+            int myInt = 5 + 5;
+            Console.WriteLine(myInt);
+
+            int myInt2 = 5 - 5;
+            Console.WriteLine(myInt2);
+
+            double myDouble = 45 / 2;
+            Console.WriteLine(myDouble);
+
+        }
+    }
+}
