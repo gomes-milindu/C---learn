d9a4b9b511bca17ecbacc10f7f4a6dd23bfc019a
Milindu Gomes <gomesmilindu@gmail.com>
2025-06-19 09:47:04 +0530

    tutorial 01 uploded

---
+++ uTube_tutorial_01/uTube_tutorial_01/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uTube_tutorial_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Set Button Clicked!";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Cancel Button Clicked!";
+        }
+    }
+}
